ce6ecce99ed430888a9c83002ecabc7735c9f829
xiaofujie369 <132354004+xiaofujie369@users.noreply.github.com>
2025-11-25 07:58:19 +0800

    Update env.js

--- env.js
+++ env.js
@@ -1,1 +1,11 @@
-// env placeholder
+window.routerBase = "https://xb.shy521.com/";
+
+window.settings = {
+  title: "可云",
+  description: "可云加速入口",
+  assets_path: "/assets",
+  theme: { color: "default" },
+  version: "1.0.0",
+  background_url: "",
+  logo: "",
+};
